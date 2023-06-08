2e00be2ed83b813eb9dd994fbee0f6f5b9418e5d
Killian <killianbld@macbook-pro-de-killian.home>
2023-06-08 20:24:24 +0200

    fin du test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,174 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import Head from "next/head";
+import axios from "axios";
 
 export default function Home() {
+  const Data1 = "Bar";
+  const Data2 = "Brasserie";
+  const Data3 = "Cave";
+  const Data4 = "⚽️ Sportif";
+  const Data5 = "🌊 Vue mer";
+  const Data6 = "🔥 Au chaud";
+
   const Title = "L'arena Brasserie";
   const Description = "Brasserie";
 
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://api.brest.bar/items/bars');
 
+      https://api.brest.bar/items/bars_categories``
+
+
+      
+      console.log('Response:', response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  fetchData();
 
   return (
     <main className=" ">
-      <Head>
-        <title>My page title</title>
-      </Head>
-      <header className=" z-50 w-full flex items-center justify-center bg-white shadow-md">
+      <header className=" fixed z-50 w-full flex items-center justify-center bg-white shadow-md">
         <div className="w-full  ">
           <div className="container mx-auto px-4 py-6">
             <div className="flex items-center justify-between">
               <h1 className="text-black text-2xl">
-                🍺 Brest{" "}
-                <span className="bg-clip-text text-transparent bg-gradient-to-t text-pink-700">
+                🍺 Brest
+                <span className="font-bold text-pink-700">
                   Bar
                 </span>
               </h1>
 
-              <a className="bg-pink-700 rounded-md p-2">🙋 Faire une demande</a>
+              <a className="bg-pink-700 rounded-full px-4 py-2 cursor-pointer">
+                🙋 Faire une demande
+              </a>
             </div>
           </div>
         </div>
       </header>
-      <section className="flex bg-white shadow-xl h-screen">
-        <section className="w-96 h-full p-10 shadow-xl  border-2 border-gray-100 rounded-xl">
+      <section className="flex bg-gray-100 shadow-xl h-screen pt-20">
+        <section className="w-96 h-full p-6  border-2   bg-white">
           <h1 className="font-bold text-2xl mb-10 text-black ">
             Trouvez le bar qu'il vous faut
             <span className="bg-clip-text text-transparent text-pink-700">
-              <br></br>selon votre humeur
+              &nbsp;selon votre humeur
             </span>
           </h1>
-          <h2 className="font-bold text-xl mb-2 text-black">
-            Où boire à Brest ?
-          </h2>
-          <div className="flex w-full items-center justify-between max-w-sm">
-            <div className="flex-col flex items-center cursor-pointer hover:bg-slate-100 p-2 rounded-md">
-              <div className="bg-white rounded-xl p-6 text-4xl shadow-xl">
-                🍹
+          <div className="py-10">
+            <h2 className="font-bold text-xl mb-2 text-black">
+              Où boire à Brest ?
+            </h2>
+            <div className="flex w-full items-center justify-between max-w-sm ">
+              <div className="flex-col flex items-center cursor-pointer hover:bg-slate-100 p-2 rounded-xl border-2 border-gray-200">
+                <div className=" rounded-xl p-4 text-3xl">🍹</div>
+                <span className="font-bold text-black">{Data1}</span>
               </div>
-              <span className="font-bold text-black">Bar</span>
-            </div>
-            <div className="flex-col flex items-center cursor-pointer hover:bg-slate-100 p-2 rounded-md ">
-              <div className="bg-white rounded-xl p-6 text-4xl shadow-xl">
-                🍺
+              <div className="flex-col flex items-center cursor-pointer hover:bg-slate-100 p-2 rounded-xl border-2 border-gray-200 ">
+                <div className=" rounded-xl p-4 text-3xl">🍺</div>
+                <span className="font-bold text-black">{Data2}</span>
               </div>
-              <span className="font-bold text-black">Brasserie</span>
-            </div>
-            <div className="flex-col flex items-center cursor-pointer hover:bg-slate-100 p-2 rounded-md">
-              <div className="bg-white rounded-xl p-6 text-4xl shadow-xl">
-                🍷
+              <div className="flex-col flex items-center cursor-pointer hover:bg-slate-100 p-2 rounded-xl border-2 border-gray-200 ">
+                <div className=" rounded-xl p-4 text-3xl">🍷</div>
+                <span className="font-bold text-black">{Data3}</span>
               </div>
-              <span className="font-bold text-black">Cave</span>
             </div>
           </div>
-          <div className="flex font-bold align-center justify-between cen pt-5">
-            <div className="text-black">Explorer</div>
-            <a className="bg-pink-700 rounded-md p-2">Filter ✍️</a>
+
+          <div className="flex font-bold align-center justify-between ">
+            <div className="text-black text-xl">Explorer</div>
+            <a className="bg-pink-700 rounded-full px-4 py-2 cursor-pointer">
+              Filter ✍️
+            </a>
           </div>
-          <div className="bg-gray-900 rounded-xl">
-          <h1>{Description}</h1>
-            <h1>{Title}</h1>
+
+          <div className="max-h-96 overflow-y-scroll">
+            <div className="bg-gray-900 rounded-xl flex p-6 mt-2 justify-between">
+              <div>
+                <h1 className="text-l">{Description}</h1>
+                <h1 className="text-2xl font-bold">{Title}</h1>
+                <div className="flex flex-wrap">
+                  <span className="bg-blue-400 p-2 rounded-full text-xs mt-2 mr-2">
+                    {Data4}
+                  </span>
+                  <span className="bg-orange-400 p-2 rounded-full text-xs mt-2 mr-2">
+                    {Data5}
+                  </span>
+                  <span className="bg-yellow-400 p-2 rounded-full text-xs mt-2 mr-2">
+                    {Data6}
+                  </span>
+                </div>
+              </div>
+              <a className="flex justify-center items-center bg-white p-2 rounded-full cursor-pointer h-16 w-16 ">
+                👀
+              </a>
+            </div>
+            <div className="bg-gray-900 rounded-xl flex p-6 mt-2 justify-between">
+              <div>
+                <h1 className="text-l">{Description}</h1>
+                <h1 className="text-2xl font-bold">{Title}</h1>
+                <div className="flex flex-wrap">
+                  <span className="bg-blue-400 p-2 rounded-full text-xs mt-2 mr-2">
+                    {Data4}
+                  </span>
+                  <span className="bg-orange-400 p-2 rounded-full text-xs mt-2 mr-2">
+                    {Data5}
+                  </span>
+                  <span className="bg-yellow-400 p-2 rounded-full text-xs mt-2 mr-2">
+                    {Data6}
+                  </span>
+                </div>
+              </div>
+              <a className="flex justify-center items-center bg-white p-2 rounded-full cursor-pointer h-16 w-16 ">
+                👀
+              </a>
+            </div>
+            <div className="bg-gray-900 rounded-xl flex p-6 mt-2 justify-between">
+              <div>
+                <h1 className="text-l">{Description}</h1>
+                <h1 className="text-2xl font-bold">{Title}</h1>
+                <div className="flex flex-wrap">
+                  <span className="bg-blue-400 p-2 rounded-full text-xs mt-2 mr-2">
+                    {Data4}
+                  </span>
+                  <span className="bg-orange-400 p-2 rounded-full text-xs mt-2 mr-2">
+                    {Data5}
+                  </span>
+                  <span className="bg-yellow-400 p-2 rounded-full text-xs mt-2 mr-2">
+                    {Data6}
+                  </span>
+                </div>
+              </div>
+              <a className="flex justify-center items-center bg-white p-2 rounded-full cursor-pointer h-16 w-16 ">
+                👀
+              </a>
+            </div>
+            <div className="bg-gray-900 rounded-xl flex p-6 mt-2 justify-between">
+              <div>
+                <h1 className="text-l">{Description}</h1>
+                <h1 className="text-2xl font-bold">{Title}</h1>
+                <div className="flex flex-wrap">
+                  <span className="bg-blue-400 p-2 rounded-full text-xs mt-2 mr-2">
+                    {Data4}
+                  </span>
+                  <span className="bg-orange-400 p-2 rounded-full text-xs mt-2 mr-2">
+                    {Data5}
+                  </span>
+                  <span className="bg-yellow-400 p-2 rounded-full text-xs mt-2 mr-2">
+                    {Data6}
+                  </span>
+                </div>
+              </div>
+              <a className="flex justify-center items-center bg-white p-2 rounded-full cursor-pointer h-16 w-16 ">
+                👀
+              </a>
+            </div>
+
+            
           </div>
         </section>
-        <section className="w-3/4 h-full bg-red-500 rounded-md"></section>
+        <section className="w-3/4 h-full bg-gray-100 rounded-md">Maps</section>
       </section>
     </main>
   );
